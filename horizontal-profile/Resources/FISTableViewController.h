1e64b3ac5cee114a710b97cddae03a14fae775d0
joshmotley <joshkmotley@gmail.com>
2015-09-11 15:44:31 -0400

    layout depends on device/screen size - proj completed

---
+++ horizontal-profile/Resources/FISTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISTableViewController.h
+//  horizontal-profile
+//
+//  Created by Joshua Motley on 9/11/15.
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISTableViewController : UITableViewController
+
+@end
